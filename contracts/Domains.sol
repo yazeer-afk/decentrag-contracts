a3b3c537a7641bbf770a3485915e128f4d73589a
yazeerr <int.yazeer@gmail.com>
2022-04-18 23:52:29 +0530

    - Implemented paying mechanism to register domains

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,22 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import {StringUtils} from "./lib/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+    string public tld;
 
-  mapping(string => address) public domains;
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
 
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
-  }
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed.", _tld);
+    }
 
-  function register(string calldata name) public {
-    domains[name] = msg.sender;
-    console.log("%s has registered a domain!", msg.sender);
-  }
+    function price(string calldata name) public pure returns (uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17;
+        } else if (len == 4) {
+            return 3 * 10**17;
+        } else {
+            return 1 * 10**17;
+        }
+    }
 
-  function getAddress(string calldata name) public view returns(address){
-    return domains[name];
-  }
+    function register(string calldata name) public payable {
+        require(domains[name] == address(0));
+
+        uint _price = price(name);
+        require(msg.value >= _price, "Not enough Matic was paid!");
+
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender, "You are not the owner");
+        records[name] = record;
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,19 @@
 const main = async () => {
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy("ninja");
   await domainContract.deployed();
   console.log("Contract deployed to: ", domainContract.address);
+
+  const txn = await domainContract.register("mortal", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await txn.wait();
+
+  const domainOwner = await domainContract.getAddress("mortal");
+  console.log("mortal Domain Owner: ", domainOwner);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const bootstrap = async () => {
