7f2f121308e0e27fbcd2ccc766dab0b6e840975a
yazeerr <int.yazeer@gmail.com>
2022-04-18 17:59:53 +0530

    implemented function to register domain names

---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy();
+  await domainContract.deployed();
+  console.log("Contract deployed to: ", domainContract.address);
+};
+
+const bootstrap = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+bootstrap();
